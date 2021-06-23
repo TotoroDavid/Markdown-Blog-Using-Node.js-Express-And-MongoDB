ecbdb8b61c5b07b4d2e6098b368eda53dc521281
TotoroDavid <davidtoro65@hotmail.com>
2021-06-23 10:25:16 -0500

    none

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 
 const app = express()
 
 mongoose.connect('mongodb://localhost/blog', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
 })
 
 app.set('view engine', 'ejs')
@@ -14,12 +16,10 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use('/articles', articleRouter)
 
-app.get('/', (req, res) => {
-    const articles = [{
-        title: 'Test Article',
-        createdAt: new Date(),
-        description: 'test description'
-    }]
+app.get('/', async(req, res) => {
+    const articles = await Article.find().sort({
+        createdAt: 'desc'
+    })
     res.render('articles/index', { articles: articles })
 })
 
