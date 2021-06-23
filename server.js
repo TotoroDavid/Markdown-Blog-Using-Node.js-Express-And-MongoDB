006431b6760cc7593c3aae2764909943e7298467
TotoroDavid <davidtoro65@hotmail.com>
2021-06-23 09:30:10 -0500

    setting the project

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const articleRouter = require('./routes/articles')
+
+const app = express()
+
+mongoose.connect('mongodb://localhost/blog', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({ extended: false }))
+app.use('/articles', articleRouter)
+
+app.get('/', (req, res) => {
+    const articles = [{
+        title: 'Test Article',
+        createdAt: new Date(),
+        description: 'test description'
+    }]
+    res.render('articles/index', { articles: articles })
+})
+
+app.listen(3000, console.log('server in port 3000'))
